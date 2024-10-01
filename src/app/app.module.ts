76e219f6d573ff8be27922e77d0d10cea68cec8b
shaun <shaun.vcure@hotmail.com>
2024-10-02 02:10:01 +0800

    added pwa, fixed setting page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { GlobalStore } from './service/global.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InjectionToken } from '@angular/core';
+import { InjectionToken, isDevMode } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 export const WINDOW = new InjectionToken<Window>('WindowToken', {
   factory: () => {
@@ -24,7 +25,13 @@ export const WINDOW = new InjectionToken<Window>('WindowToken', {
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    HttpClientModule
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
     provideClientHydration(),
