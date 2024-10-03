2c290a897e62f6236894481b0df697bb16104188
shaun <shaun.vcure@hotmail.com>
2024-10-03 16:09:29 +0800

    logon page : added phone code list for country

--- src/app/pages/logon/logon.page.ts
+++ src/app/pages/logon/logon.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { GlobalStore } from '../../service/global.service';
+import PhoneCode from '../../../assets/json/phone-code.json';
 
 @Component({
   templateUrl: './logon.page.html',
@@ -9,6 +10,22 @@ import { GlobalStore } from '../../service/global.service';
 export class LogonPage implements OnInit {
 
   'page' : string = 'login';
+  'phoneCode' : any = PhoneCode;
+  'counrtyData' : any = [];
+  'loginForm' : any = {
+    'identified' : '',
+    'password' : ''
+  }
+  'registerForm' : any = {
+    'display_name' : '',
+    'username'     : '',
+    'dob'          : '',
+    'email'        : '',
+    'country'      : '',
+    'dial_code'    : ''
+  }
+  'searchValue' : string = '';
+  'showCountry' : boolean = false;
 
   constructor(
     private route: ActivatedRoute,
@@ -17,7 +34,42 @@ export class LogonPage implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.counrtyData = this.phoneCode;
+  }
 
+  closeCountry()
+  {
+    this.showCountry = false;
+    this.searchValue = '';
+    this.counrtyData = this.phoneCode;
+  }
+  searchCountry(event:any)
+  {
+    this.searchValue = event;
+    if(this.searchValue === '')
+    {
+      this.counrtyData = this.phoneCode;
+    }
+    else
+    {
+      let arr : any = [];
+      this.phoneCode.map((ele:any) =>
+      {
+        let lowCap : any = ele.name.toLowerCase();
+        if(ele.dial_code.includes(this.searchValue) || ele.name.includes(this.searchValue) || lowCap.includes(this.searchValue))
+        {
+          arr.push(ele);
+        }
+      });
+
+      this.counrtyData = arr;
+    }
+  }
+  selectCode(data:any)
+  {
+    this.registerForm.country = data.name;
+    this.registerForm.dial_code = data.dial_code;
+    this.closeCountry();
   }
 
 
